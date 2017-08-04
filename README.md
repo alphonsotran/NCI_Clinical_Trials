d8981fe569b83e6e046eb3b8f29e3375ff6e7296
Alphonso Tran <email": null>
2017-08-03 17:44:03 -0700

    ADD NCI_adapater and helper method.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NCI_Clinical_Trials
+
+https://clinicaltrialsapi.cancer.gov/#!/cancer45clinical45trials/searchTrialsByGet
---
+++ app/helpers/NCI_adapter.rb
@@ -0,0 +1,22 @@
+module NCIClinicalTrials
+
+  def self.clinical_trial_search
+    require 'uri'
+    require 'net/http'
+
+    #Interpolate data into this URI
+    url = URI("https://clinicaltrialsapi.cancer.gov/v1/clinical-trials?sites.org_state_or_province=CA&sites.org_state_or_province=OR")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["cache-control"] = 'no-cache'
+    request["postman-token"] = 'a0993ea2-f4c2-fb35-7c63-3ae82d91e94b'
+
+    response = http.request(request)
+    puts response.read_body
+  end
+
+end
